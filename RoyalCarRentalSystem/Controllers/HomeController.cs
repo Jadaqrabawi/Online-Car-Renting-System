b336541e1f1228e7262a7794dfb602be69c02900
jadaqrabawi <jadaqrabawi7@gmail.com>
2025-03-08 13:22:12 +0300

    first commit

---
+++ RoyalCarRentalSystem/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RoyalCarRentalSystem.Models;
+using System.Data;
+using System.Data.Entity;
+
+namespace RoyalCarRentalSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult Car()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult  Features()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(PassengerData passengerData)
+        {
+            using(RoyalCarDataEntities db=new RoyalCarDataEntities())
+            {
+                if(ModelState.IsValid)
+                {
+                    db.PassengerDatas.Add(passengerData);
+                    db.SaveChanges();
+                    ViewBag.mesaage = "Registration Successfully";
+                    ModelState.Clear();
+                }
+            }
+            return View(passengerData);
+        }
+    }
+}
---
+++ RoyalCarRentalSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RoyalCarRentalSystem.MvcApplication" Language="C#" %>
